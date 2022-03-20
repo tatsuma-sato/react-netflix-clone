ef8e755270bcf04606b7c650a01749a3767b3953
tatsuma-sato <t.tsuma.s@gmail.com>
2022-03-20 13:33:58 -0700

    added accordion and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import JumbotronContainer from "./containers/jumbotron";
+import JumbotronContainer from "./containers/Jumbtron.container";
+import FooterContainer from "./containers/Footer.container";
+import FaqsContainer from "./containers/Faqs.container";
 
 function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
 }
 
 export default App;
